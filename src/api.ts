4b4f7f27eacf6ad02c1ab8007db331aa38adc568
KOMMINITHIN <nithin_kommi@srmap.edu.in>
2025-09-25 19:32:49 +0530

    Ups2

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // Use environment variable for API base URL
-const BASE_URL = 'http://localhost:7082';
+//const BASE_URL = 'http://localhost:7082';
+const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:7082';
 
 const api = axios.create({
   baseURL: BASE_URL,
